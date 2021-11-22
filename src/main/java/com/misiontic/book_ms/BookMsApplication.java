f9bace0711c798c854b78090641ee132b3c22445
Josue Nuñez <josuenunezprada@gmail.com>
2021-11-21 19:01:50 -0500

    spring initializer

---
+++ src/main/java/com/misiontic/book_ms/BookMsApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.book_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMsApplication.class, args);
+	}
+
+}
