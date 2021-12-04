2df2e73cc2da2b819aa816bb765032400770fac6
María Angélica Garzón Alfonso <94009469+MarAngelGarz@users.noreply.github.com>
2021-12-04 16:10:37 -0500

    Update BookRepository.java

--- src/main/java/com/misiontic/book_ms/repositories/BookRepository.java
+++ src/main/java/com/misiontic/book_ms/repositories/BookRepository.java
@@ -7,4 +7,8 @@ import java.util.List;
 
 public interface BookRepository extends MongoRepository<Book, Integer>{
     List<Book> findByIdOwner(int idOwner);
+    List<Book> findByTittle (String tittle);
+    List<Book> findByAuthor (String author);
+    List<Book> findByGenre (String genre);
+    List<Book> findByEditorial (String editorial);
 }
